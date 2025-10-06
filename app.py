4f3a204f6a6cb3618894466d70542c0a55c6d563
Arijit Chowdhury <143516210+student-Arijit@users.noreply.github.com>
2025-10-06 10:47:44 +0000

    update font and design

--- app.py
+++ app.py
@@ -6,10 +6,16 @@ from io import BytesIO
 import base64
 import os
 from PyPDF2 import PdfReader, PdfWriter
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+
+# Register Raleway Bold
+pdfmetrics.registerFont(TTFont('raleway', 'assets/Fonts/raleway.ttf'))
+pdfmetrics.registerFont(TTFont('BAHNSCHRIFT', 'assets/Fonts/BAHNSCHRIFT.ttf'))
 
 st.title("Cover page generating Engine")
 univ=st.selectbox("Enter your University/college: ", ("University of Calcutta"))
-stream=st.selectbox("Enter Your Stream: ", ("B.Sc", "B.com", "B.A"), index=0)
+stream=st.selectbox("Enter Your Stream: ", ("B.Sc Honours", "B.com", "B.A"), index=0)
 sem=st.selectbox("Enter your semester: ", ("I", "II", "III", "IV", "V", "VI", "VII", "VIII"), index=3)
 roll=st.text_input("Enter Your Roll No.: ")
 reg=st.text_input("Enter Your Reg No.: ")
@@ -32,12 +38,45 @@ if st.button("Generate PDF"):
     p.progress(4)
     darkblue = colors.Color(17/255, 17/255, 132/255)
     c.setFillColor(darkblue)
-    c.drawString(400, height - 230, f"{univ}")
-    c.drawString(100, height - 100, f"Name: {sem}")
-    c.drawString(100, height - 140, f"Score: {stream}")
+
+    #Stream
+    c.setFont("Helvetica-Bold", 20) 
+    c.drawString(50, height - 185, stream)
+
+    #Semester
+    c.drawString(60, height - 220, f"Semester - {sem}")
+
+    #roll, reg, paper name, UNIV NAME section
+    if univ == "University of Calcutta":
+        #univ name
+        c.setFont("raleway", 30)
+        c.drawString(370, height - 230, "UNIVERSITY") 
+        c.drawString(430, height - 260, "OF")
+        c.drawString(380, height - 290, "CALCUTTA")
+
+        #roll
+        c.setFont("BAHNSCHRIFT", 20)
+        c.drawString(30, height - 320, "C.U. ROLL NO: - ")
+        c.setFillColor(colors.black)
+        c.drawString(170, height - 320, roll)
+
+        #reg
+        c.setFillColor(colors.blue)
+        c.drawString(30, height - 370, "C.U. REG. NO: - ")
+        c.setFillColor(colors.black)
+        c.drawString(170, height - 370, reg)
+
+        #paper name
+        c.setFillColor(colors.blue)
+        c.drawString(30, height - 420, "PAPER NAME: - ")
+        c.setFillColor(colors.black)
+        c.drawString(170, height - 420, paper_name)
+        c.setFillColor(colors.blue)
+
+    c.setFont("Helvetica-Bold", 16)
     c.drawString(50, 190, f"SUBJECT: {sub}")
     c.drawString(30, 147, f"PAPER CODE: {paper_code}")
-    c.drawImage("assets/University_of_Calcutta_logo.png", 450, height - 125, width=100, height=90, mask="auto")
+    c.drawImage("assets/University_of_Calcutta_logo.png", 380, height - 170, width=150, height=140, mask="auto")
     c.save()
     p.progress(50)
     packet.seek(0)
@@ -58,7 +97,7 @@ if st.button("Generate PDF"):
     output.write(final_buffer)
     final_buffer.seek(0)
     p.progress(100)
-
+    
     st.download_button(
         label="📄 Download PDF",
         data=final_buffer,
