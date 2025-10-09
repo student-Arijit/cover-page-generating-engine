39344f0e462c0bae2befd0b3c359914f35b0e056
Arijit Chowdhury <143516210+student-Arijit@users.noreply.github.com>
2025-10-09 19:11:18 +0000

    app updating

--- app.py
+++ app.py
@@ -5,16 +5,39 @@ st.set_page_config(
     "cover-page-generating-engine",
     page_icon = "📘"
 )
+st.title("Cover Page Generating Engine:")
+st.subheader("Information Form:")
 
-st.title("Cover page generating Engine")
-univ=st.selectbox("Enter your University/college: ", ("University of Calcutta"))
-stream=st.selectbox("Enter Your Stream: ", ("B.Sc Honours", "B.com", "B.A"), index=0)
-sem=st.selectbox("Enter your semester: ", ("I", "II", "III", "IV", "V", "VI", "VII", "VIII"), index=3)
-roll=st.text_input("Enter Your Roll No.: ")
-reg=st.text_input("Enter Your Reg No.: ")
-paper_name=st.text_input("Enter Your Paper name: ")
-sub=st.text_input("Enter Your Subject: ")
-paper_code=st.text_input("Enter Your Paper Code: ")
+st.error("developers are working, no functionality happens sorry😔")
+
+with st.form("About Form", clear_on_submit = False):
+    st.subheader("Student About:")
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        univ=st.selectbox("Enter your University/college: ", ("University of Calcutta"))
+        stream=st.selectbox("Enter Your Stream: ", ("B.Sc Honours", "B.com", "B.A"), index=0)
+
+    with col2:
+        sem=st.selectbox("Enter your semester: ", ("I", "II", "III", "IV", "V", "VI", "VII", "VIII"), index=3)
+        roll=st.text_input("Enter Your Roll No.: ")
+        reg=st.text_input("Enter Your Reg No.: ")
+
+    st.subheader("Paper Details:")
+    paper_name=st.text_input("Enter Your Paper name: ")
+    sub=st.text_input("Enter Your Subject: ")
+    paper_code=st.text_input("Enter Your Paper Code: ")
+
+    col1, col2, col3 = st.columns([1, 0.5, 1])
+
+    with col2:
+        submit = st.form_submit_button("Submit")
+
+    if submit:
+        st.success("Your Form submitted. Please, choose your design and download.")    
+
+st.subheader("Choose your designs here:")
 
 col1, col2, col3 = st.columns(3)
 
