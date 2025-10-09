c8ea7b875c9e897aeb853ea83fdcfce8032d68d1
Arijit Chowdhury <143516210+student-Arijit@users.noreply.github.com>
2025-10-09 17:33:33 +0000

    template2 import

---
+++ components/template2.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import A4
+from reportlab.lib import colors
+from io import BytesIO
+import base64
+from PyPDF2 import PdfReader, PdfWriter
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+
